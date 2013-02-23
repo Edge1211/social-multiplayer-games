6c68626f611961bae88e4ce93611bbf0115ccc03
Zhihan Li <lizhihan1211@gmail.com>
2013-02-22 22:39:36 -0500

    hw 2

---
+++ hw2_5/StateExplorerImpl.java
@@ -0,0 +1,25 @@
+package org.zhihanli.hw2_5;
+
+import java.util.*;
+import org.shared.chess.*;
+import org.shared.chess.StateExplorer;
+
+public class StateExplorerImpl implements StateExplorer{
+	@Override
+	  public Set<Move> getPossibleMoves(State state) {
+	    // TODO
+	    return null;
+	  }
+
+	  @Override
+	  public Set<Move> getPossibleMovesFromPosition(State state, Position start) {
+	    // TODO
+	    return null;
+	  }
+
+	  @Override
+	  public Set<Position> getPossibleStartPositions(State state) {
+	    // TODO
+	    return null;
+	  }
+}
